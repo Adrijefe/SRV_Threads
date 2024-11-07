f030c1f3c4c783ce340d722b263ccc7a921f4431
serra <adrpeiser@alu.edu.gva.es>
2024-11-07 11:29:32 +0100

    para poder hacer el 4 en clase

---
+++ src/Ej1.java
@@ -0,0 +1,19 @@
+public class Ej1 {
+    public static void main(String[] args) throws InterruptedException {
+        var mithread = Thread.startVirtualThread(()->{
+            int i = 0;
+            while(true) System.out.println(i++);
+            });
+
+        var mithread2 = Thread.startVirtualThread(()->{
+            int i = 0;
+            while(true) System.out.println("Hola cara  culo");
+        });
+
+        mithread.join();
+        mithread2.join();
+
+
+
+        }
+    }
---
+++ src/Ej2.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+
+public class Ej2 {
+        public static void main(String[] args) throws InterruptedException, ExecutionException {
+            try (var executor = Executors.newVirtualThreadPerTaskExecutor()){
+                var weather = executor.submit(() ->  fetch("https://apipa.vercel.app/api/w/weather"));
+
+                var temperature = executor.submit(() -> fetch("https://apipa.vercel.app/api/w/temperature"));
+
+                var wind = executor.submit(() -> fetch("https://apipa.vercel.app/api/w/wind"));
+
+                var moon  = executor.submit(() -> fetch("https://apipa.vercel.app/api/w/moon"));
+                System.out.println(weather.get());
+                System.out.println(temperature.get());
+                System.out.println(wind.get());
+                System.out.println(moon.get());
+
+            }
+
+
+
+        }
+
+        static String fetch(String url) {
+            try(var inputStream = new URI(url).toURL().openStream()) {
+                return new String(inputStream.readAllBytes());
+            } catch (IOException | URISyntaxException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
